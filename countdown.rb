4fd18028c4a31eaec01906eeaa615fd9d631afad
Ryan Daley <rdaley321@yahoo.com>
2018-10-29 17:01:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,10 @@ def countdown(integer)
 end
 
 def countdown_with_sleep(integer)
-  
+  while integer > 0 
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
